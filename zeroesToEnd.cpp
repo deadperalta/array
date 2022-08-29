531acb6ac701976a5cd0b56ae72bd5cc131bdea3
deadperalta <86057977+deadperalta@users.noreply.github.com>
2022-08-29 23:43:31 +0530

    Update zeroesToEnd.cpp

--- zeroesToEnd.cpp
+++ zeroesToEnd.cpp
@@ -1,15 +1,41 @@
-void zeroesToEnd(int arr[])
-{
-  int end=n-1;
-  int i=0;
-  
-  while(i < end)
-  {
-    if(arr[i] == 0)
-    {
-      swap(arr[i],arr[end]);
-      end--;
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	void pushZerosToEnd(int arr[], int n) {
+	    int j=0;
+	    for(int i=0;i<n;i++)
+	    {
+	        if(arr[i] != 0)
+	        {
+	            swap(arr[i],arr[j]);
+	            j++;
+	        }
+	    }
+	}
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.pushZerosToEnd(arr, n);
+        for (i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
     }
-    i++;
-  }
-}
+    return 0;
+}  // } Driver Code Ends
