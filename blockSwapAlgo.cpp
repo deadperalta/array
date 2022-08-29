096d45cea7cf72c48ae8d039d3a7de4538da8dd5
deadperalta <86057977+deadperalta@users.noreply.github.com>
2022-08-29 07:30:23 +0530

    Create blockSwapAlgo.cpp

---
+++ blockSwapAlgo.cpp
@@ -0,0 +1,34 @@
+// C++ code for above implementation
+void leftRotate(int arr[], int d, int n)
+{
+	int i, j;
+	if (d == 0 || d == n)
+		return;
+	
+	/* If number of elements to be rotated is more than array size*/
+	if(d > n)
+	d = d % n;
+	
+	i = d;
+	j = n - d;
+	while (i != j)
+	{
+		if (i < j) /*A is shorter*/
+		{
+			swap(arr, d - i, d + j - i, i);
+			j -= i;
+		}
+		else /*B is shorter*/
+		{
+			swap(arr, d - i, d, j);
+			i -= j;
+		}
+		
+		// printArray(arr, 7);
+	}
+	
+	/*Finally, block swap A and B*/
+	swap(arr, d - i, d, i);
+}
+
+// This code is contributed by Shivani
