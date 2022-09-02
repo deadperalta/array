50a824cbdfaa099c75627a5cef7d90a55a80f69b
deadperalta <86057977+deadperalta@users.noreply.github.com>
2022-09-02 22:58:32 +0530

    Create pairSumInUnsortedArray.cpp

---
+++ pairSumInUnsortedArray.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Function to check if array has 2 elements
+	// whose sum is equal to the given value
+	bool hasArrayTwoCandidates(int arr[], int n, int x) {
+	    unordered_map<int,int>freq;
+	    
+	    for(int i=0;i<n;i++)
+	    {
+	        int key=arr[i];
+	        
+	        if(freq.find(x-key) != freq.end())
+	        {
+	            return true;
+	        }
+	        /*if(freq.find(key-x) != freq.end())
+	        {
+	            return true;
+	        }*/
+	        freq[arr[i]]++;
+	    }
+	    return false;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.hasArrayTwoCandidates(arr, n, x);
+        cout << (ans ? "Yes\n" : "No\n");
+    }
+    return 0;
+}
+
+// } Driver Code Ends
